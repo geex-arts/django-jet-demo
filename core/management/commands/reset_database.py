b66c7b6e7719be00eeea18fa255b1505dd4110b2
Denis K <f1nal@cgaming.org>
2015-08-28 15:53:28 +0300

    Add reset management command

---
+++ core/management/commands/reset_database.py
@@ -0,0 +1,9 @@
+from django.core.management import call_command, BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Resets database'
+
+    def handle(self, *args, **options):
+        call_command('flush', '--no-input')
+
