2c8f2d5c2c67746dd6374e55c944bbf0b8e9a7eb
Denis K <f1nal@cgaming.org>
2015-09-11 16:12:30 +0300

    Make user important field read only

--- core/admin.py
+++ core/admin.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class MyUserAdmin(UserAdmin):
     list_per_page = 5
     date_hierarchy = 'date_joined'
+    readonly_fields = ('username', 'is_superuser', 'is_staff', 'is_active')
 
 admin.site.unregister(User)
 admin.site.register(User, MyUserAdmin)
