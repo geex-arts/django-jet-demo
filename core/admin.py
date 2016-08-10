b95cef7c3ff3fe5348c72eefe8a2d4a5f516e2bb
Denis K <f1nal@cgaming.org>
2016-08-11 00:51:42 +0300

    Add filter horizontal demo

--- core/admin.py
+++ core/admin.py
@@ -7,6 +7,7 @@ class MyUserAdmin(UserAdmin):
     list_per_page = 5
     date_hierarchy = 'date_joined'
     readonly_fields = ('username', 'is_superuser', 'is_staff', 'is_active')
+    filter_horizontal = ('user_permissions',)
 
 admin.site.unregister(User)
 admin.site.register(User, MyUserAdmin)
