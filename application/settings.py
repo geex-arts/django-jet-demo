ce5cf6e61c0bfd885a62d12c60d14c1ac8cbcf08
Denis K <f1nal@cgaming.org>
2016-08-12 18:24:49 +0300

    Fix template override

--- application/settings.py
+++ application/settings.py
@@ -77,7 +77,7 @@ MIDDLEWARE_CLASSES = (
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
